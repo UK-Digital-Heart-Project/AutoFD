d939097d1d10d43bd11af3ff893ec7ce6a083abf
ptokarcz <37544040+ptokarcz@users.noreply.github.com>
2018-05-02 10:49:54 +0100

    File-merging function added for MacOS.

---
+++ pft_MergeFilesInMacOS.m
@@ -0,0 +1,27 @@
+function pft_MergeFilesInMacOS(Home, Away)
+
+% Home is the main MATLAB working directory, Away is the top-level folder where o/p is directed
+cd(Away);
+
+% List all the local CSV files
+Listing = dir('Data*.csv');
+Entries = { Listing.name };
+Folders = [ Listing.isdir ];
+Entries = Entries(~Folders);
+Entries = sort(Entries); 
+
+% Create and run a merge command
+a = 'cat ';
+b = sprintf('%s ', Entries{:});
+c = '> Summary-Auto-FD-v0-1.csv 2>/dev/null';
+
+Cmd = [ a b c ];
+
+disp(Cmd);
+
+system(Cmd);
+
+% Return to the Home directory
+cd(Home);
+
+end
