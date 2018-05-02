0e7b4f68c3b69101ea6e9e80fa38373f8cc7e149
ptokarcz <37544040+ptokarcz@users.noreply.github.com>
2018-05-02 10:30:33 +0100

    Functions added to merge o/p CSV files programmatically.

---
+++ pft_MergeFilesInWindows.m
@@ -0,0 +1,27 @@
+function pft_MergeFilesInWindows(Home, Away)
+
+% Home is the main MATLAB working directory, Away is the top-level folder where o/p is directed
+cd(Away);
+
+% List all the local CSV files
+Listing = dir('Data*.csv');
+Entries = { Listing.name };
+Folders = [ Listing.isdir ];
+Entries = Entries(~Folders);
+Entries = sort(Entries); 
+
+% Create and run a merge command
+a = 'type ';
+b = sprintf('%s ', Entries{:});
+c = '> Summary-Auto-FD-v0-1.csv 2>nul';
+
+Cmd = [ a b c ];
+
+system(Cmd);
+
+disp(Cmd);
+
+% Return to the Home directory
+cd(Home);
+
+end
